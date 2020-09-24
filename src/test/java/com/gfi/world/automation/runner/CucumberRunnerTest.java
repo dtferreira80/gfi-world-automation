c4554d0e6967117a1c9a3fb215f618936f61b886
Diogo Ferreira <dtferreira@gmail.com>
2020-09-24 21:23:46 +0100

    alterando tag

--- src/test/java/com/gfi/world/automation/runner/CucumberRunnerTest.java
+++ src/test/java/com/gfi/world/automation/runner/CucumberRunnerTest.java
@@ -11,7 +11,7 @@ import org.junit.runner.RunWith;
     features = {"src/test/resources"},
     glue = {"com.gfi.world.automation.stepDefinitions"},
     plugin = {"io.qameta.allure.cucumberjvm.AllureCucumberJvm"},
-    tags = {"not @notImplemented"})
+    tags = {"~@notImplemented"})
 public class CucumberRunnerTest {
 
   @BeforeClass
